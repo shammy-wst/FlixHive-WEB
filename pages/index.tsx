fd3eee8db74f2023b905d395dc143da1c1ae602c
Samy951 <samy.hamlat@etu.univ-paris1.fr>
2023-04-24 17:09:18 +0200

    Added API images, title, release date with grids

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
-import Auth from "@/pages/Auth";
+import Navbar from "@/pages/components/Navbar";
 import axios from "axios";
 import {server} from "@/config/index";
+import PopularMovie from "@/pages/components/PopularMovie";
+
 
 export default function Home({movies}){
   console.log(movies);
   return (
-      <Auth/>
-  )
+      <div className={"bg-zinc-900"}>
+      <Navbar/>
+      <PopularMovie movies={movies.results}/>;
+    </div>
+)
 }
 export async function getStaticProps (){
   const res = await axios(`${server}/popular?api_key=${process.env.API_KEY}&language=fr-FR&page=1`)
